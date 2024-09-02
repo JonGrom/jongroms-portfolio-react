dd99ff591ef0031e87a41fac825aeba3896466c8
EnzoAndSo <grommeshjonathan@gmail.com>
2024-09-02 14:08:17 -0500

    built file structure. Created component files and pages. Wrote App.jsx to import all components and use router-dom to write page routes

---
+++ src/components/pages/AboutMePage.jsx
@@ -0,0 +1,6 @@
+export default function AboutMePage(props){
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/components/pages/PortfolioPage.jsx
@@ -0,0 +1,6 @@
+export default function PortfolioPage(props){
+    return(
+        <>
+        </>
+    )
+}
