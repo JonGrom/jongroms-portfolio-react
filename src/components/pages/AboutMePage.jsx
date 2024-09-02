377fdb487bac0c8fb3db03d7a09de1523ef265e5
EnzoAndSo <grommeshjonathan@gmail.com>
2024-09-02 16:54:25 -0500

    added aboutme and portfolio content, unstyled

--- src/components/pages/AboutMePage.jsx
+++ src/components/pages/AboutMePage.jsx
@@ -1,7 +1,14 @@
 export default function AboutMePage(props){
     return(
         <>
-        <h1>ABOUTME</h1>
+            <h1>ABOUTME</h1>
+            <div class="headshot">
+                <img src="../../../assets/images/photo-of-me.jpg" alt="A Recent Picture of Myself."/>
+            </div>
+            <section id="about-me">
+                <h2>Jonathan Grommesh...</h2>
+                <p>is my name. Jon Grom for short. I am a soon-to-be certified full-stack web developer, specializing in MERN, as well as a fiction writer, musical director, and aspiring neuroscientist, pursuing my bachelors degree at the Univerty of Minnesota: Twin Cites. I indulge in a variety of hobbies, including but not limited to: piano playing, board games, volleyball, bouldering, reading, writing, and theatre. In a world of specialists I am a generalist, and have found that the skills I pick up from one domain serve me across all others in thrillingly unexpected ways. This has served me well in my several managerial service roles and freelance musical directing career. My one passion, if I were forced to choose, would not just be learning, but applying my knowledge beyond its initial scope and onward towards new horizons.</p>
+            </section>
         </>
     )
 }
--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -1,7 +1,40 @@
 export default function PortfolioPage(props){
     return(
         <>
-        <h1>PORTFOLIO</h1>
+            <h1>PORTFOLIO</h1>
+            <div id="projects">
+                <h2>Projects</h2>
+                <div class="top-project">
+                    <h3>HTML Semantics</h3>
+                    <a href="https://jongrom.github.io/mod1-semantics/">
+                        <img src="./assets/images/html.jpg" alt="a keyboard and coffee beans"/>
+                    </a>
+                </div>
+                <div class="bottom-projects">
+                    <h3>Placeholder1</h3>
+                    <a href="./assets/misc./placeholder.html">
+                        <img src="./assets/images/placeholder1.jpg" alt="A top down view of a casual work meeting around large wooden table."/> 
+                    </a>
+                </div>
+                <div class="bottom-projects">
+                    <h3>Placeholder2</h3> 
+                    <a href="./assets/misc./placeholder.html">
+                        <img src="./assets/images/placeholder2.jpg" alt="A close-up of a laptop running VS.code."/>
+                    </a>
+                </div>
+                <div class="bottom-projects">
+                    <h3>Placeholder3</h3>
+                    <a href="./assets/misc./placeholder.html">
+                        <img src="./assets/images/placeholder3.jpg" alt="A tesla ball toy in a dark room."/>
+                    </a>
+                </div>
+                <div class="bottom-projects">
+                    <h3>Placeholder4</h3>
+                    <a href="./assets/misc./placeholder.html">
+                        <img src="./assets/images/placeholder4.jpg" alt="A sleek toy drone against a foggy gray backdrop."/>
+                    </a>
+                </div>
+            </div>
         </>
     )
 }
