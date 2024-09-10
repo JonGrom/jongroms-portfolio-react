6ecb3a7929710d6430c998324e2d044536e0033b
EnzoAndSo <grommeshjonathan@gmail.com>
2024-09-10 18:19:35 -0500

    completed logic in form errors, began constructing resume page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,63 @@
 export default function Project(props){
+//create an array of projects to rendere on page
+    const menu = [
+        {
+            title: 'War',
+            imageSrc: NULL,
+            href: 'https://floatingpoint-exaflop.github.io/group-project-one',
+            githubLink: 'https://github.com/floatingpoint-exaflop/group-project-one',
+            desc: 'javascript',
+        },
+        {
+            title: 'Weather Board',
+            imageSrc: NULL,
+            href: 'https://jongrom.github.io/jongroms-weather-board/',
+            githubLink: 'https://github.com/JonGrom/jongroms-weather-board',
+            desc: 'javascript',
+        },
+        {
+            title: 'Exposition Inn',
+            imageSrc: NULL ,
+            href: 'https://exposition-inn.onrender.com/',
+            githubLink: 'https://github.com/JonGrom/Exposition_Inn',
+            desc: 'NodeJS/Express/SQL',
+        },
+        {
+            title: 'Everybody Eats',
+            imageSrc: NULL ,
+            href: '!!!!',
+            githubLink: 'https://github.com/TeutonicTed/project-03',
+            desc: 'MERN stack',
+        },
+        {
+            title: 'Content Manager',
+            imageSrc: NULL ,
+            href: 'https://youtu.be/hafRNX_U2DA',
+            githubLink: 'https://github.com/JonGrom/jongroms-content-management-system',
+            desc: 'NodeJS/PostgreSQL',
+        },
+        {
+            title: 'Social Media Back-End',
+            imageSrc: NULL ,
+            href: 'https://youtu.be/TPiI7oBYPUU',
+            githubLink: 'https://github.com/JonGrom/jongroms-social-media-backend',
+            desc: 'MongoDB/Express',
+        },
+        
+    ]
+
     return(
         <>
+            <div className="col-6">
+                {menu.map( item => (
+                    <div className="flex">
+                        <h6>{item.title}</h6>
+                    </div>
+                ))}
+            </div>
+            <div className="col-6">
+
+            </div>
         </>
     )
 }
--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -4,21 +4,32 @@ export default function ContactPage(props){
     //TODO: make form
     //{fields: name, email, message}
     const [ inputObj, setInputObj ] = useState({name: '', email: '', message: ''})
-    const [warningObj, setWarningObj] = useState({})
     const [ invalidMsg, setInvalidMsg ] = useState('')
+    const [ message, setMessage ] = useState({name: '', email: '', message: ''})
 
     //update input object as user types
     function handleInputChange(event){
         //set input object to user input
-        setInputObj({...inputObj, [event.target.name]: event.target.value})
-        console.log(inputObj[event.target.name])
+        // setInputObj({...inputObj, [event.target.name]: event.target.value})
+
+        setInputObj( (prev) => {
+            const newState = {...prev, [event.target.name]: event.target.value }
+            return newState
+        })
+        if(event.target.value.length>0){
+            setMessage({...message, [event.target.name]: ""})
+        }
     }
 
     //Warn user that field is required when cursor leaves
 
-    function mouseLeaveWarn(event){
+    function handleBlur(event){
+        if(!inputObj[event.target.name].length){
+            setMessage({...message, [event.target.name]: 'This field cannot be empty'})
+        }
         
     }
+
     //validate form on submit
     function validateForm(){
         const regex = /.+\@.+\..+/
@@ -27,13 +38,13 @@ export default function ContactPage(props){
         } else if (!inputObj.email.match(regex)){
             setInvalidMsg('Invalid email')
         } else {
+            setInvalidMsg('Message sent!')
             handleSubmit()
         }
-        console.log(invalidMsg)
     }
 
     function handleSubmit(){
-        console.log('FORM SUBMITTED!')
+        setInputObj({name: '', email: '', message: ''})
     }
 
 
@@ -42,16 +53,28 @@ export default function ContactPage(props){
             <h4>Contact</h4>
             <form className="col-4 d-flex flex-column">
                 <label className="pt-2" htmlFor="name">Name:</label>  
-                <input value={inputObj.name} type="text" id="name" autoComplete="name" onChange={handleInputChange} /> 
+                <input value={inputObj.name} type="text" name="name" autoComplete="name" onChange={handleInputChange} onBlur={handleBlur} />
+                {message.name.length>0 && (
+                    <span className="warn">{message.name}</span>
+                )}   
 
                 <label className="pt-2" htmlFor="email">Email:</label>
-                <input value={inputObj.email} type="text" id="email" autoComplete="email" onChange={handleInputChange} />
+                <input value={inputObj.email} type="text" name="email" autoComplete="email" onChange={handleInputChange} onBlur={handleBlur}/>
+                {message.email.length>0 && (
+                    <span className="warn">{message.email}</span>
+                )}
 
                 <label className="pt-2" htmlFor="message">Message:</label> 
-                <textarea value={inputObj.message} name="message" id="message" onChange={handleInputChange} />
+                <textarea value={inputObj.message} name="message" id="message" onChange={handleInputChange} onBlur={handleBlur}/>
+                {message.message.length>0 && (
+                    <span className="warn">{message.message}</span>
+                )}
                 
-                <div className="pt-2 pb-2">
-                    <input type="button" value="Send" id="btn" onClick={() => validateForm(inputObj)}/>  
+                <div className="pt-2 pb-2   ">
+                    <input type="button" value="Send" id="btn" onClick={() => validateForm(inputObj)}/> 
+                    {invalidMsg.length>0 && (
+                         <span className="warn">{invalidMsg}</span>
+                    )} 
                 </div>
             </form>
 
