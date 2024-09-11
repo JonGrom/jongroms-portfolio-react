9e3bb93e9028274a5893ca407ec72807692ebb09
EnzoAndSo <grommeshjonathan@gmail.com>
2024-09-11 18:24:52 -0500

    footer built with links to youtube linkedin and github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer(props){
     return(
         <>
-        <h1>FOOTER</h1>
+        <footer>
+            <div className="d-flex justify-content-between p-4">
+            <a href="https://github.com/JonGrom">github</a>
+            <a href="https://www.linkedin.com/in/jonathan-grommesh-420406328/">linkedin</a>
+            <a href="https://www.youtube.com/@jonathangrommesh">youtube</a>
+            </div>
+        </footer>
         </>
     )
 }
